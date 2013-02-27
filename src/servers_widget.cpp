9eed578c2847f0f26d29b8430ca80bee247e9109
apavlov at home <intser79@gmail.com>
2013-02-27 09:04:57 +0600

    Add splitters

--- src/servers_widget.cpp
+++ src/servers_widget.cpp
@@ -4,6 +4,9 @@ servers_widget::servers_widget(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+    QList<int> sz;
+    sz << 500 << 150;
+    vsplit->setSizes(sz);
 }
 
 servers_widget::~servers_widget()
