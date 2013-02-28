90a6c3d11c644eec257a65c165841d88cd2e625e
apavlov <intser79@gmail.com>
2013-02-28 15:23:43 +0600

    Store control sizes

--- src/servers_widget.cpp
+++ src/servers_widget.cpp
@@ -1,14 +1,27 @@
 #include "servers_widget.h"
+#include "preferences.h"
 
 servers_widget::servers_widget(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
-    QList<int> sz;
-    sz << 500 << 150;
-    vsplit->setSizes(sz);
+    Preferences pref;
+
+    if (!hsplit->restoreState(pref.value("ServersWidget/HSplitter").toByteArray()))
+    {
+        QList<int> sz;
+        sz << 500 << 100;
+        hsplit->setSizes(sz);
+    }
+
+    vsplit->restoreState(pref.value("ServersWidget/VSplitter").toByteArray());
+    tableServers->horizontalHeader()->restoreState(pref.value("ServersWidget/TableServers").toByteArray());
 }
 
 servers_widget::~servers_widget()
 {
+    Preferences pref;
+    pref.setValue("ServersWidget/VSplitter", vsplit->saveState());
+    pref.setValue("ServersWidget/HSplitter", hsplit->saveState());
+    pref.setValue("ServersWidget/TableServers", tableServers->horizontalHeader()->saveState());
 }
