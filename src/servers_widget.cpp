32d8d0f2e54c2f530579bb74e2b52d76e5192e10
apavlov <intser79@gmail.com>
2013-03-01 22:10:13 +0600

    Implement menus

--- src/servers_widget.cpp
+++ src/servers_widget.cpp
@@ -1,4 +1,5 @@
 #include <QMenu>
+#include <QSortFilterProxyModel>
 #include "servers_widget.h"
 #include "preferences.h"
 #include "servers_table_model.h"
@@ -7,6 +8,38 @@ servers_widget::servers_widget(QWidget *parent) :
     QWidget(parent), m_smodel(NULL)
 {
     setupUi(this);
+    // prepare menus
+    m_connect_menu = new QMenu(this);
+    m_disconnect_menu = new QMenu(this);
+
+    //fileDownload->setIcon(QIcon(":/emule/search/Download.png"));
+    m_connect_action = new QAction(this);
+    m_connect_action->setText(tr("Connect to"));
+    m_connect_action->setIcon(QIcon(":/emule/common/ConnectDo.ico"));
+    m_disconnect_action = new QAction(this);
+    m_disconnect_action->setText(tr("Disconnect from"));
+    m_disconnect_action->setIcon(QIcon(":/emule/common/ConnectStop.ico"));
+    m_remove_action = new QAction(this);
+    m_remove_action->setText(tr("Remove"));
+    m_remove_action->setIcon(QIcon(":/emule/common/DeleteSelected.ico"));
+    m_remove_all_action = new QAction(this);
+    m_remove_all_action->setText(tr("Remove all"));
+    m_remove_all_action->setIcon(QIcon(":/emule/common/DeleteAll.ico"));
+
+    m_connect_menu->addAction(m_connect_action);
+    m_connect_menu->addSeparator();
+    m_connect_menu->addAction(m_remove_action);
+    m_connect_menu->addAction(m_remove_all_action);
+
+    m_disconnect_menu->addAction(m_connect_action);
+    m_disconnect_menu->addSeparator();
+    m_disconnect_menu->addAction(m_remove_action);
+    m_disconnect_menu->addAction(m_remove_all_action);
+
+    connect(m_connect_action, SIGNAL(triggered()), this, SLOT(connect_slot()));
+    connect(m_disconnect_action, SIGNAL(triggered()), this, SLOT(disconnect_slot()));
+    connect(m_remove_action, SIGNAL(triggered()), this, SLOT(remove_slot()));
+    connect(m_remove_all_action, SIGNAL(triggered()), this, SLOT(removeAll_slot()));
 
     m_smodel = new servers_table_model(this);
     m_smodel->load();
@@ -24,14 +57,17 @@ servers_widget::servers_widget(QWidget *parent) :
         hsplit->setSizes(sz);
     }
 
+    tableServers->horizontalHeader()->setContextMenuPolicy(Qt::CustomContextMenu);
     vsplit->restoreState(pref.value("ServersWidget/VSplitter").toByteArray());
-    //tableServers->horizontalHeader()->restoreState(pref.value("ServersWidget/TableServers").toByteArray());
+    tableServers->horizontalHeader()->restoreState(pref.value("ServersWidget/TableServers").toByteArray());
 
     connect(tableServers->horizontalHeader(), SIGNAL(sortIndicatorChanged(int, Qt::SortOrder)),
             this, SLOT(serversSortChanged(int, Qt::SortOrder)));
 
     connect(tableServers->horizontalHeader(), SIGNAL(customContextMenuRequested(const QPoint&)),
             this, SLOT(displayHeaderMenu(const QPoint&)));
+
+    connect(tableServers, SIGNAL(customContextMenuRequested(const QPoint&)), this, SLOT(displayPopupMenu(const QPoint&)));
 
     tableServers->horizontalHeader()->setSortIndicator(servers_table_model::DC_NAME, Qt::AscendingOrder);
 }
@@ -106,3 +142,28 @@ void servers_widget::switchAddBtn()
     }
 }
 
+void servers_widget::connect_slot()
+{
+
+}
+
+void servers_widget::disconnect_slot()
+{
+
+}
+
+void servers_widget::remove_slot()
+{
+
+}
+
+void servers_widget::removeAll_slot()
+{
+
+}
+
+
+void servers_widget::on_tableServers_customContextMenuRequested(const QPoint &pos)
+{
+    m_connect_menu->exec(QCursor::pos());
+}
--- src/servers_widget.h
+++ src/servers_widget.h
@@ -2,10 +2,12 @@
 #define SERVERS_WIDGET_H
 
 #include <QWidget>
-#include <QSortFilterProxyModel>
 #include "ui_servers_widget.h"
 
 class servers_table_model;
+class QSortFilterProxyModel;
+class QMenu;
+class QAction;
 
 class servers_widget : public QWidget, private Ui::servers_widget
 {
@@ -13,8 +15,14 @@ class servers_widget : public QWidget, private Ui::servers_widget
 private:
     QSortFilterProxyModel*  m_sort_model;
     servers_table_model*    m_smodel;
+    QMenu*                  m_connect_menu;
+    QMenu*                  m_disconnect_menu;
 
-    void displayHeaderMenu(const QPoint&);
+    QAction*                m_connect_action;
+    QAction*                m_disconnect_action;
+    QAction*                m_remove_action;
+    QAction*                m_remove_all_action;
+
     void switchAddBtn();
 public:
     explicit servers_widget(QWidget *parent = 0);
@@ -24,6 +32,13 @@ private slots:
     void on_btnAdd_clicked();
     void on_editIP_textChanged(const QString &arg1);
     void on_editName_textChanged(const QString &arg1);
+    void displayHeaderMenu(const QPoint&);
+
+    void connect_slot();
+    void disconnect_slot();
+    void remove_slot();
+    void removeAll_slot();
+    void on_tableServers_customContextMenuRequested(const QPoint &pos);
 };
 
 #endif // SERVERS_WIDGET_H
