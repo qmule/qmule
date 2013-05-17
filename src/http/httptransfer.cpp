eed539fa6e1a1944297bb2490ed67ac66aff19b6
Ivan Sharov <isharov@che-d-113.(none)>
2013-05-17 16:35:41 +0600

    http header for file transfer

--- src/http/httptransfer.cpp
+++ src/http/httptransfer.cpp
@@ -15,28 +15,41 @@ void HttpTransfer::run()
 
   if (srcFile.open(QIODevice::ReadOnly))
   {
-    while(!m_interrupted && !srcFile.atEnd())
+    buf += "POST /push1_pub?id=game HTTP/1.1\r\n";
+    buf += "User-Agent: qMule\r\n";
+    buf += "Host: localhost\r\n";
+    buf += "Accept: */*\r\n";
+    buf += "Content-Type: application/octet-stream\r\n";
+    buf += QString("Content-Length: %1\r\n\r\n").arg(srcFile.size());
+
+    if (m_dst->write(buf) != -1 && m_dst->waitForBytesWritten())
     {
-      buf = srcFile.read(256 * 1024);
-      if (buf.size() == 0)
+      while(!m_interrupted && !srcFile.atEnd())
       {
-        qDebug() << "Error: cannot read file: " << m_srcPath;
-        break;
-      }
-      if (m_dst->write(buf) == -1 || !m_dst->waitForBytesWritten())
-      {
-        qDebug() << "Error: cannot send file: " << m_srcPath;
-        break;
+        buf = srcFile.read(256 * 1024);
+        if (buf.size() == 0)
+        {
+          qDebug() << "Error: cannot read file: " << m_srcPath;
+          break;
+        }
+        if (m_dst->write(buf) == -1 || !m_dst->waitForBytesWritten())
+        {
+          qDebug() << "Error: cannot send file: " << m_srcPath;
+          break;
+        }
       }
     }
+    else
+      qDebug() << "Error: cannot send header: " << m_srcPath;
 
-    qDebug() << "File upload completed: " << m_srcPath;
+    if (srcFile.atEnd())
+      qDebug() << "File upload completed: " << m_srcPath;
+
     srcFile.close();
+    m_dst->disconnectFromHost();
   }
   else
-  {
     qDebug() << "Error: cannot open file: " << m_srcPath;
-  }
 }
 
 void HttpTransfer::interrupt()
